daed44fd017a2ea25ca62458305a739c04ad120d
Vladimir <volodymyrkalashnyk@gmail.com>
2017-04-10 12:22:50 +0300

    first commit

---
+++ gitTestProject/src/main/java/com/HelloWorld.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by User on 10.04.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
