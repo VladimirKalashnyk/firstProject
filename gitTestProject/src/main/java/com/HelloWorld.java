aa96e72c78107d7c97c1a92d8c634019a89d3d88
Vladimir <volodymyrkalashnyk@gmail.com>
2017-04-10 15:08:02 +0300

    second commit

--- gitTestProject/src/main/java/com/HelloWorld.java
+++ gitTestProject/src/main/java/com/HelloWorld.java
@@ -6,5 +6,10 @@ package com;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello!!!");
     }
 }
